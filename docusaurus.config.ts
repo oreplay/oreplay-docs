8d985e19143ecd1b9dce1eb88b62aace17279446
Guillem Serrallonga <pi.matherror@gmail.com>
2026-07-04 11:56:15 +0200

    Initial docusaurus config, old en-docu migrated and es-docu planned, new structure set up.

---
+++ docusaurus.config.ts
@@ -0,0 +1,175 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'O-Replay documentation',
+  tagline: 'By orienteers, For orienteers',
+  favicon: 'img/logo.svg',
+  // favicon: 'img/favicon.ico',
+
+  // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
+  future: {
+    v4: true, // Improve compatibility with the upcoming Docusaurus v4
+  },
+
+  // Set the production url of your site here
+  url: 'https://docs.oreplay.es',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'oreplay', // Usually your GitHub org/user name.
+  projectName: 'O-Replay', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        /*blog: {
+          showReadingTime: true,
+          feedOptions: {
+            type: ['rss', 'atom'],
+            xslt: true,
+          },
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          // editUrl:
+          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // Useful options to enforce blogging best practices
+          onInlineTags: 'warn',
+          onInlineAuthors: 'warn',
+          onUntruncatedBlogPosts: 'warn',
+        },*/
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    // image: 'img/docusaurus-social-card.jpg',
+    image: 'img/logo.png',
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'O-Replay',
+      logo: {
+        alt: 'O-Replay Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: "Organizer's info",
+        },
+        {to: '/features', label: 'Features', position: 'left'},
+        {to: '/about-us', label: 'About Us', position: 'left'},
+        /*{to: '/blog', label: 'Blog', position: 'left'},*/
+        {
+          href: 'https://www.oreplay.es',
+          label: 'O-Replay Events',
+          position: 'left',
+        },
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/oreplay/',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: "Introduction for Organizers",
+              to: '/docs/intro',
+            },
+            {
+              label: "Getting Started",
+              to: '/docs/category/getting-started',
+            },
+            {
+              label: "Uploading Data",
+              to: '/docs/category/uploading-data',
+            },
+            {
+              label: "Managing Errors",
+              to: '/docs/category/managing-errors',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'About Us',
+              to: '/about-us',
+            },
+            {
+              label: 'Features',
+              to: '/features',
+            },
+            {
+              label: 'O-Replay',
+              href: 'https://www.oreplay.es',
+            },
+          ],
+        },
+        {
+          title: 'Contribute',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/oreplay/',
+            },
+            {
+              label: 'Weblate',
+              href: 'https://hosted.weblate.org/projects/o-replay/',
+            }
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} O-Replay documentation. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
---
+++ src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,71 @@
+import type {ReactNode} from 'react';
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: ReactNode;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Easy to Use',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        O-Replay was designed from the ground up to display orienteering live results
+        and be easily used by competitors, organizers and spectators.
+      </>
+    ),
+  },
+  {
+    title: 'Focus on What Matters',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        O-Replay lets you focus on the competition, while we do the chores. Go
+        ahead and create your orienteering events with clean results.
+      </>
+    ),
+  },
+  {
+    title: 'Powered by orienteers',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+        As orienteers ourselves, we understand your needs. Help us create
+        the results' website of the future for all orienteering disciplines.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): ReactNode {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
